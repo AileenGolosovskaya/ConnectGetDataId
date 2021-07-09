70c0eacc8d06f1453af5f938ee197fb2de22a942
AileenGolosovskaya <86467680+AileenGolosovskaya@users.noreply.github.com>
2021-07-09 15:58:21 +0530

    Add files via upload

---
+++ SoftwareUpdateShareID/app_ad_code_id.php
@@ -0,0 +1,21 @@
+{
+	"ad_id":
+		[
+			{
+			"g_ad":"1",
+			"fb_ad":"0",
+			"adpub_id":"pub-9469599640054086",
+			"adapp_id":"ca-app-pub-9469599640054086~1571340348",
+			"adappopen_id":"ca-app-pub-9469599640054086/7116145214",
+			"adbanner_id":"ca-app-pub-9469599640054086/8000290979",
+			"adbanner_rect_id":"ca-app-pub-9469599640054086/9550736864",
+			"adinterstial_id":"ca-app-pub-9469599640054086/7864807029",
+			"adnativebanner_id":"ca-app-pub-9469599640054086/4873125258",
+			"adfbbanner_id":"IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
+			"adfbinterstial_id":"YOUR_PLACEMENT_ID",
+			"adfbnative_id":"YOUR_PLACEMENT_ID",
+			"adfbnative_banner_id":"YOUR_PLACEMENT_ID",		
+			"adfbrectangle_banner_id":"YOUR_PLACEMENT_ID"
+			}
+		]
+}
