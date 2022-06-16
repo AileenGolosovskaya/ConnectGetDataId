cd7023e095e5d1c43c85cb8f935617c8b567ce44
AileenGolosovskaya <86467680+AileenGolosovskaya@users.noreply.github.com>
2022-06-16 17:11:13 +0530

    Add files via upload

--- SpeedTestAppMonitorID/app_ad_code_id.php
+++ SpeedTestAppMonitorID/app_ad_code_id.php
@@ -2,8 +2,8 @@
 	"ad_id":
 		[
 			{
-			"g_ad":"1",
-			"fb_ad":"0",
+			"g_ad":"0",
+			"fb_ad":"1",
 			"g_ad_admob":"0",
 			"adpub_id":"pub-3940256099942544",
 			"adapp_id":"ca-app-pub-3940256099942544~3347511713",
@@ -13,11 +13,11 @@
 			"adinterstial_id":"/21952429235,22644075336/TTS_WOXXIN_2_5GSPEEDTESTANDAPPMONITOR_INTERSTITIAL",
 			"adnativebanner_id":"/21952429235,22644075336/TTS_WOXXIN_2_5GSPEEDTESTANDAPPMONITOR_NATIVE_AD",
 			"adnativebanner_admobid":"ca-app-pub-9469599640054086/6991747744",
-			"adfbbanner_id":"IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
-			"adfbinterstial_id":"YOUR_PLACEMENT_ID",
-			"adfbnative_id":"YOUR_PLACEMENT_ID",
-			"adfbnative_banner_id":"YOUR_PLACEMENT_ID",		
-			"adfbrectangle_banner_id":"YOUR_PLACEMENT_ID"
+			"adfbbanner_id":"4964163773600538_4964166403600275",
+			"adfbinterstial_id":"4964163773600538_4964168476933401",
+			"adfbnative_id":"4964163773600538_4964172136933035",
+			"adfbnative_banner_id":"4964163773600538_4964172933599622",		
+			"adfbrectangle_banner_id":"4964163773600538_4964170743599841"
 			}
 		]
 }
